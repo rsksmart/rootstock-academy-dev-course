6125c1c9d92d59bbabd65709109c3f0c08e67640
Sebastian G <sebastian.guaqueta@rootstocklabs.com>
2025-11-06 11:47:19 +0100

    test/ adding failing test

--- module5/contracts/OneMilNftPixels.sol
+++ module5/contracts/OneMilNftPixels.sol
@@ -113,14 +113,13 @@ contract OneMilNftPixels is ERC721, Ownable, IERC1363Receiver {
         require(balance >= compensationBalance, 'Insufficient balance!');
         require(compensationBalance > 0, 'Insufficient compensation balance!');
 
-        // SECURITY FIX OMP-001: Zero out balance BEFORE external call (checks-effects-interactions pattern)
-        compensationBalances[_msgSender()] = 0;
-
         // transfer msg.sender's compensation LUNAs to the address specified in `to`. If caller is EOA, call ERC20 transfer()
         bool withdrawalSuccess = (_msgSender() == tx.origin)
             ? acceptedToken.transfer(address(to), compensationBalance) // EOA
             : acceptedToken.transferAndCall(address(to), compensationBalance); // SC
         require(withdrawalSuccess, 'withdraw failed');
+        // SECURITY HINT: modify this
+        compensationBalances[_msgSender()] = 0;
 
         emit WithdrawCompensation(address(to), compensationBalance);
     }
@@ -241,33 +240,27 @@ contract OneMilNftPixels is ERC721, Ownable, IERC1363Receiver {
      * param _data Additional data with no specified format
      */
     function _transferReceived(
-        address _sender,
-        uint256 _amount,
+        address /* _sender */,
+        uint256 /* _amount */,
         bytes memory _data
     ) private {
         (
             bytes4 selector,
-            ,
+            address newOwner,
             uint24 pixelId,
             bytes3 colour,
             uint256 amount
         ) = abi.decode(_data, (bytes4, address, uint24, bytes3, uint256));
-        
-        // SECURITY FIX OMP-002: Validate that the amount in data matches actual transferred amount
-        require(amount == _amount, 'Amount mismatch');
-        
-        // SECURITY FIX OMP-003: Only allow buy() and update() functions, prevent arbitrary function calls
-        require(
-            selector == this.buy.selector || selector == this.update.selector,
-            'Call of an unknown function'
+        // SECURITY HINT: modify this
+        bytes memory callData = abi.encodeWithSelector(
+            selector,
+            newOwner,
+            pixelId,
+            colour,
+            amount
         );
-        
-        // Direct call instead of delegatecall
-        if (selector == this.buy.selector) {
-            buy(_sender, pixelId, colour, amount);
-        } else if (selector == this.update.selector) {
-            update(_sender, pixelId, colour, amount);
-        }
+        (bool success, ) = address(this).delegatecall(callData);
+        require(success, 'Function call failed');
     }
 
     receive() external payable {
@@ -278,3 +271,4 @@ contract OneMilNftPixels is ERC721, Ownable, IERC1363Receiver {
         revert('Unknown function call');
     }
 }
+
