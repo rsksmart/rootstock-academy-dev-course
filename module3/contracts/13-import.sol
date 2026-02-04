6f3773b12e062032fc23118267c91e516dbe7c6f
Rsync25 <rsync252@gmail.com>
2026-02-04 11:12:32 -0300

    fix

--- module3/contracts/13-import.sol
+++ module3/contracts/13-import.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Import statement - specific import from relative path
+import {ISuperHonk} from "./13-import-target.sol";
+
 contract Cars {
-    enum CarStatus { driving, parked }
+    enum CarStatus { 
+        driving, 
+        parked 
+    }
 
     event CarHonk(uint256 indexed carId);
+    event CarAdded(uint256 indexed carId, address owner, bytes3 colour, uint8 doors);
+    event StatusChanged(uint256 indexed carId, CarStatus newStatus);
 
     struct Car {
         bytes3 colour;
@@ -13,8 +21,15 @@ contract Cars {
         address owner;
     }
 
+    // Using the imported ISuperHonk type
+    ISuperHonk public superHonk;
     uint256 public numCars = 0;
     mapping(uint256 => Car) public cars;
+
+    constructor(address _superHonkAddress) {
+        // Initialize with imported interface type
+        superHonk = ISuperHonk(_superHonkAddress);
+    }
 
     function addCar(
         bytes3 colour,
@@ -36,6 +51,9 @@ contract Cars {
             msg.sender
         );
         cars[carId] = newCar;
+        
+        emit CarAdded(carId, msg.sender, colour, doors);
+        return carId;
     }
 
     function statusChange(
@@ -44,12 +62,15 @@ contract Cars {
     )
         public
         onlyOwner(carId)
+        carExists(carId)
     {
         require(
             cars[carId].status != newStatus,
             "no change"
         );
         cars[carId].status = newStatus;
+        
+        emit StatusChanged(carId, newStatus);
     }
 
     function honk(
@@ -58,19 +79,48 @@ contract Cars {
     )
         public
         onlyOwner(carId)
+        carExists(carId)
     {
         emit CarHonk(carId);
-        // No external contract call
+        
+        // Using the imported interface
+        if (isLoud && address(superHonk) != address(0)) {
+            superHonk.honk();  // Call through imported interface
+        }
     }
 
-    modifier onlyOwner(
-        uint256 carId
-    )
+    function updateSuperHonk(address newSuperHonkAddress) public {
+        require(msg.sender == cars[1].owner, "only first car owner");
+        superHonk = ISuperHonk(newSuperHonkAddress);
+    }
+
+    // Get car details
+    function getCar(uint256 carId) 
+        public 
+        view 
+        carExists(carId)
+        returns(bytes3 colour, uint8 doors, CarStatus status, address owner) 
     {
+        Car memory car = cars[carId];
+        return (car.colour, car.doors, car.status, car.owner);
+    }
+
+    // Modifiers
+    modifier onlyOwner(uint256 carId) {
         require(
             cars[carId].owner == msg.sender,
             "only owner"
         );
         _;
+    }
+    
+    modifier carExists(uint256 carId) {
+        require(carId > 0 && carId <= numCars, "car does not exist");
+        _;
+    }
+    
+    // Function to demonstrate interface usage
+    function testInterface() public view returns (bool) {
+        return address(superHonk) != address(0);
     }
 }
