24a44023d3150dd75529e77f768de7d744fd313a
Ad-Capital <Adxkap@gmail.com>
2026-02-07 14:33:39 +0100

    Complete Module 3 exercises

--- module3/contracts/01-contract.sol
+++ module3/contracts/01-contract.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-___ solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-___ Abc {
+contract Abc {
 
-    ___() {
+    constructor () {
     }
 
 }
