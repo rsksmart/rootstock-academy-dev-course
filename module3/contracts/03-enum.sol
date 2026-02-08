6d0aae85210036d5d913bb60cffbd4b0663c3fc7
arowolo kehinde <arowolokehinde321@gmaul.com>
2026-02-08 19:07:35 +0100

    completed module 3

--- module3/contracts/03-enum.sol
+++ module3/contracts/03-enum.sol
@@ -2,11 +2,14 @@ pragma solidity ^0.8.0;
 
 contract Cars {
 
-    ___ CarStatus __________
+    enum CarStatus { 
+        parked, 
+        driving
+        }
 
     bytes3 public colour;
     uint8 public doors;
-    ___ ___ status;
+    CarStatus public status;
     address public owner;
 
     constructor() {
