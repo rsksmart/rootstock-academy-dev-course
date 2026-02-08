b87d4e054be19c88f99c1656fbac541f5b72a9ef
kpj2006 <24ucs074@lnmiit.ac.in>
2026-02-09 01:59:32 +0530

    04-struct.sol done

--- module3/contracts/04-struct.sol
+++ module3/contracts/04-struct.sol
@@ -1,16 +1,22 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract Cars {
 
     enum CarStatus { driving, parked }
 
-    ___ Car {
-        ___ colour;
-        ___ doors;
-        ___ status;
-        ___ owner;
+    struct Car {
+        bytes3 colour;
+        uint8 doors;
+        CarStatus status;
+        address owner;
     }
 
+    // When moving variables into struct 
+    // Remove all visibility modifiers inside struct
+    // Apply visibility to struct variable instead like below
+    Car public myCar;
+    // Solidity auto-generates getter for public struct
     constructor() {
     }
 
