5bd0518c4197ef240530e56fe2265640a3f74977
Mikemushunje <Michaelmushunje1@gmail.com>
2026-02-11 12:09:12 +0200

    Add files via upload

---
+++ contracts/02-primitive-variables.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Cars {
+
+    bool public isBlack;
+    uint256 public doors;
+    address public owner;
+
+    constructor() {
+    }
+
+}
