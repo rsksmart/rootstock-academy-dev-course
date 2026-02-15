6d47aac5ce7ab8d335bd758c67c275ac44e1f905
Pat Blk <goodnewsshow@yahoo.com>
2026-02-15 12:54:04 -0500

    Define Car struct with properties in 04-struct.sol

--- module3/contracts/04-struct.sol
+++ module3/contracts/04-struct.sol
@@ -5,11 +5,17 @@ contract Cars {
     enum CarStatus { driving, parked }
 
     ___ Car {
+    struct Car {
         ___ colour;
+        bytes3 colour;
         ___ doors;
+        uint8 doors;
         ___ status;
+        CarStatus status;
         ___ owner;
+        address owner;
     }
+Car public myCar;
 
     constructor() {
     }
