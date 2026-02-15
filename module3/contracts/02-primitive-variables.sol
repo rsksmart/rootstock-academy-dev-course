d61c2fa5fa25ab3fa164e8d70e67190b5c3d0c77
Pat Blk <goodnewsshow@yahoo.com>
2026-02-15 12:47:25 -0500

    Define public variables for Cars contract

--- module3/contracts/02-primitive-variables.sol
+++ module3/contracts/02-primitive-variables.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.0;
 contract Cars {
 
     ___ ___ isBlack;
+    bool public isBlack;
+
     ___ ___ doors;
+    uint256 public doors;
+
     ___ ___ owner;
+    address public owner;
 
     constructor() {
     }
