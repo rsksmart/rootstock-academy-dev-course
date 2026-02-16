1aecc5d51f14ce6619af39f72e43db16cfadb42f
Ivan Ursulovic <ivan@Ivans-MacBook-Air.local>
2026-02-16 02:36:34 +0100

    Complete deployment, function, and event tests

--- module4/test/03-testing-events.ts
+++ module4/test/03-testing-events.ts
@@ -21,41 +21,41 @@ describe("03-testing-events: Testing Events and Logs", function () {
 
   it("should emit event with correct carId", async function () {
     const [owner] = await ethers.getSigners();
-    
+
     // First car should have ID 1
     await expect(cars.addCar("0xff0000", 4))
-      .to.___(___, "___")
-      .withArgs(___, owner.___);
-      
+      .to.emit(cars, "CarAdded")
+      .withArgs(1, owner.address);
+
     // Second car should have ID 2
     await expect(cars.addCar("0x00ff00", 2))
       .to.emit(cars, "CarAdded")
-      .withArgs(___, owner.address);
+      .withArgs(2, owner.address);
   });
 
   it("should emit CarRemoved event", async function () {
     // Add a car first
     await cars.addCar("0xff0000", 4);
-    
+
     // Remove it and check event
-    await expect(cars.___(__))
-      .to.emit(___, "CarRemoved")
+    await expect(cars.removeCar(1))
+      .to.emit(cars, "CarRemoved")
       .withArgs(1);
   });
 
   it("should emit multiple events correctly", async function () {
     const [owner] = await ethers.getSigners();
-    
+
     // Add first car
     await expect(cars.addCar("0xff0000", 4))
       .to.emit(cars, "CarAdded")
       .withArgs(1, owner.address);
-      
+
     // Add second car
     await expect(cars.addCar("0x00ff00", 2))
       .to.emit(cars, "CarAdded")
-      .withArgs(___, ___);
-      
+      .withArgs(2, owner.address);
+
     // Add third car
     await expect(cars.addCar("0x0000ff", 5))
       .to.emit(cars, "CarAdded")
@@ -63,4 +63,3 @@ describe("03-testing-events: Testing Events and Logs", function () {
   });
 
 });
-
