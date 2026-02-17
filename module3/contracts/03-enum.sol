c46191c06f0e381784f391f96f3205e2208e39a6
Pat Blk <goodnewsshow@yahoo.com>
2026-02-16 21:47:24 -0500

    Update Solidity version and modify CarStatus enum

--- module3/contracts/03-enum.sol
+++ module3/contracts/03-enum.sol
@@ -1,12 +1,16 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.28;
 
 contract Cars {
 
-    ___ CarStatus __________
-
+    enum CarStatus{
+        driving,
+        parked
+}
     bytes3 public colour;
     uint8 public doors;
-    ___ ___ status;
+    CarStatus public status;
     address public owner;
 
     constructor() {
