d7bcaaeb061d2529a65ec25a8851883387b72a51
Pat Blk <goodnewsshow@yahoo.com>
2026-02-16 21:46:08 -0500

    Update Solidity version and constructor syntax

--- module3/contracts/01-contract.sol
+++ module3/contracts/01-contract.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
-___ solidity ^0.8.0;
+    
+    pragma solidity ^0.8.28;
+    
+    contract Abc {
 
-___ Abc {
-
-    ___() {
+    constructor() {
     }
 
 }
