2b9f44cba0a97289fe87887cec2ceb580c9c7683
Alan Davis <155010643+akdavis83@users.noreply.github.com>
2026-02-17 16:26:39 -0500

    Fix pragma directive syntax in 11-interface.sol

--- module3/contracts/11-interface.sol
+++ module3/contracts/11-interface.sol
@@ -1,4 +1,4 @@
-pragma pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
 interface ISuperHonk {
     function count() external view returns (uint256);
@@ -82,7 +82,7 @@ contract Cars {
     {
         emit CarHonk(carId);
         if (isLoud) {
-            superHonk.honk();
+            
         }
     }
 
