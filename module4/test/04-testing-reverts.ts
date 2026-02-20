18ba1b7a5d8a82095a0ca90516604da1a9f20bd0
MayowaObisesan <mayowaobi74@gmail.com>
2026-02-20 20:28:40 +0100

    module4/amtheblessed

--- module4/test/04-testing-reverts.ts
+++ module4/test/04-testing-reverts.ts
@@ -10,24 +10,24 @@ describe("04-testing-reverts: Testing Error Handling", function () {
     const Cars = await ethers.getContractFactory("Cars");
     cars = await Cars.deploy();
     await cars.waitForDeployment();
-    
+
     [owner, otherUser] = await ethers.getSigners();
   });
 
   it("should revert when non-owner tries to remove car", async function () {
     // Add a car as owner
     await cars.addCar("0xff0000", 4);
-    
+
     // Try to remove as other user - should revert
     await expect(
-      cars.___(otherUser).removeCar(1)
+      cars.connect(otherUser).removeCar(1)
       // This is Not Owner
-    ).to.be.revertedWith("___");
+    ).to.be.revertedWith("Not owner");
   });
 
   it("should revert when removing non-existent car", async function () {
     // Try to remove car that doesn't exist
-    await ___(
+    await expect(
       cars.removeCar(999)
       // This is Car does not exist
     ).to.be.revertedWith("Car does not exist");
@@ -36,20 +36,20 @@ describe("04-testing-reverts: Testing Error Handling", function () {
   it("should NOT revert when owner removes their own car", async function () {
     // Add a car
     await cars.addCar("0xff0000", 4);
-    
+
     // Owner should be able to remove it
-    await expect(cars.removeCar(1)).to.not.be.___();
+    await expect(cars.removeCar(1)).to.not.be.reverted;
   });
 
   it("should revert with correct error messages", async function () {
     // Add a car as owner
     await cars.addCar("0xff0000", 4);
-    
+
     // Test specific error message
     await expect(
       cars.connect(otherUser).removeCar(1)
-    ).to.be.___("Not owner");
-    
+    ).to.be.revertedWith("Not owner");
+
     // Test another error message
     await expect(
       cars.removeCar(999)
@@ -60,17 +60,16 @@ describe("04-testing-reverts: Testing Error Handling", function () {
     // Add two cars
     await cars.addCar("0xff0000", 4);
     await cars.addCar("0x00ff00", 2);
-    
+
     // Other user can't remove first car
     await expect(
       cars.connect(otherUser).removeCar(1)
     ).to.be.revertedWith("Not owner");
-    
+
     // Other user can't remove second car either
     await expect(
-      cars.connect(___).removeCar(2)
+      cars.connect(otherUser).removeCar(2)
     ).to.be.revertedWith("Not owner");
   });
 
 });
-
