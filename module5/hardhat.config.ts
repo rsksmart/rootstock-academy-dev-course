8a0a0fab07c80a455c80856b87309932e19be2ea
Etette Etok <etettetok5@gmail.com>
2026-02-23 11:07:07 +0100

    module5/etettetok5@gmail.com

--- module5/hardhat.config.ts
+++ module5/hardhat.config.ts
@@ -2,7 +2,13 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      evmVersion: "cancun",
+    },
+  },
 };
+
 
 export default config;
