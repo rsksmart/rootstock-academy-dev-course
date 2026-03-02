b7df2133467ad4155e3578e1fab72dde8e1894a3
cmm25 <craigmutegi@gmail.com>
2026-03-02 14:53:13 +0300

    feat: add deployment scripts and configuration for SimpleToken, PriceOracle, and NFTMarketplace; update .gitignore to include typechain-types

--- module6/hardhat.config.ts
+++ module6/hardhat.config.ts
@@ -16,7 +16,7 @@
  * - RSK Networks: https://developers.rsk.co/rsk/node/configure/
  */
 
-import { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig } from "hardhat";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
@@ -61,26 +61,26 @@ const config: HardhatUserConfig = {
         },
 
         // TODO: Configure localhost network
-        // localhost: {
-        //     url: "http://127.0.0.1:8545",
-        //     chainId: 31337
-        // },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+            chainId: 31337
+        },
 
         // TODO: Configure RSK Testnet
-        // rskTestnet: {
-        //     url: RSK_TESTNET_RPC,
-        //     chainId: 31,
-        //     accounts: [PRIVATE_KEY],
-        //     gasPrice: 60000000  // 0.06 gwei - RSK uses lower gas prices
-        // },
+        rskTestnet: {
+            url: RSK_TESTNET_RPC,
+            chainId: 31,
+            accounts: [PRIVATE_KEY],
+            gasPrice: 60000000  // 0.06 gwei - RSK uses lower gas prices
+        },
 
         // TODO: Configure RSK Mainnet
-        // rskMainnet: {
-        //     url: RSK_MAINNET_RPC,
-        //     chainId: 30,
-        //     accounts: [PRIVATE_KEY],
-        //     gasPrice: 60000000
-        // }
+        rskMainnet: {
+            url: RSK_MAINNET_RPC,
+            chainId: 30,
+            accounts: [PRIVATE_KEY],
+            gasPrice: 60000000
+        }
     },
 
     // ============================================
