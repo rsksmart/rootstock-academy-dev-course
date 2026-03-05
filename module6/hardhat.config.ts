5cbbde567119be380ebf486bf0af746190d3f0ff
Victor Aguilar <veaz.24@gmail.com>
2026-03-05 13:52:25 +0100

    exercise 4: done, and deployed on tesnet

--- module6/hardhat.config.ts
+++ module6/hardhat.config.ts
@@ -24,8 +24,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // Hardhat default key #0
+// const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // Hardhat default key #0
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 // 2: Define RPC URLs
 const RSK_TESTNET_RPC = process.env.RSK_TESTNET_RPC || "https://public-node.testnet.rsk.co";
