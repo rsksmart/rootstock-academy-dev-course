1bb006ad8e076de3abf75cea56e72799972dda0b
jayzalani <151207678+jayzalani@users.noreply.github.com>
2026-03-09 15:30:28 +0530

    module5/jaizalani

--- module5/hardhat.config.ts
+++ module5/hardhat.config.ts
@@ -2,7 +2,12 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+  version: "0.8.24",
+  settings: {
+    evmVersion: "cancun"
+  }
+}
 };
 
 export default config;
