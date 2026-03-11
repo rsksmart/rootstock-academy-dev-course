d4f725f42b76d5de517e8e0a229d2179c2b8b82d
Spoorthi-Hokrana <hokranaspoorthi4@gmail.com>
2026-03-11 14:32:28 +0530

    Fix vulnerabilities in OneMilNftPixels for Module 5

--- module5/hardhat.config.ts
+++ module5/hardhat.config.ts
@@ -2,7 +2,17 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      evmVersion: "cancun"
+    }
+  },
+  networks: {
+    hardhat: {
+      hardfork: "cancun"
+    }
+  }
 };
 
 export default config;
